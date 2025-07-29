ba76d99716189d30ab0888aa3520bc20bf43b5ff
pgrinsell <phillip.grinsell3@gmail.com>
2025-07-29 17:44:37 -0400

    fps cap of 60 to not blow up my computer

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.time.Clock().tick(60)
+    dt = 0
     while True:
         screen.fill((0, 0, 0))  
         pygame.display.flip()
+        dt += 60 / 1000
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
